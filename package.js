bacf6b32ccf432a1f73a1db341522e01a2aec71c
David Thomas Grübl <me@davidgruebl.com>
2015-12-08 17:14:26 +0200

    fixes

--- package.js
+++ package.js
@@ -15,7 +15,7 @@ module.exports = function (pkg, {buildsystem, styles, features}) {
     'gulp-useref': '^3.0.0'
   }
 
-  const npmScripts = {
+  const npmBuild = {
     'browserify': '12.0.1',
     'watchify': '3.6.1',
     'babelify': '7.2.0'
@@ -28,13 +28,13 @@ module.exports = function (pkg, {buildsystem, styles, features}) {
 
   Object.assign(
     pkg.devDependencies,
-    {
+    buildsystem.npm && {
       rimraf: '2.4.4',
       mkdirp: '0.5.1',
       'npm-run-all': '1.3.3'
     },
     styles.sass && {'node-sass': '3.4.2'},
-    buildsystem.gulp ? gulp : npmScripts,
+    buildsystem.gulp ? gulp : npmBuild,
     buildsystem.gulp && styles.sass && {
       'gulp-plumber': '^1.0.1',
       'gulp-sass': '^2.0.0'
@@ -44,8 +44,8 @@ module.exports = function (pkg, {buildsystem, styles, features}) {
     }
   )
 
-  if (!pkg.browserify.transform) pkg.browserify.transform = []
-  if (buildsystem.npm && styles.sass) pkg.browserify.transform.push('sassify')
+
+  if (buildsystem.npm && styles.sass) _.set(pkg, ‘browserify.transform’, ['sassify'])
 
   Object.assign(
     pkg.scripts,
